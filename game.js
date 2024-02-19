48293ed9e13022cd086f9cdc399fcca4aa514669
Kshitij Chowdhary <131965273+Kshitij14ry@users.noreply.github.com>
2024-02-20 00:07:35 +0530

    Update game.js

--- game.js
+++ game.js
@@ -78,5 +78,5 @@ function startover(){
 }
 
 $(".rules").click(function(){
-    alert("##Step-1: Press any button to start the game. ##Step-2: Click on the flashing box. ##Step-3: Starting from the first box, keep selecting the flashing boxes in the order, everytime when a new box flashes. ##Step-4: Remember the sequence and maintain it to keep progressing through levels. ##Eg: ('Red') then ('Red' 'Blue') then ('Red' 'Blue' 'Green') and so on.. GOOD LUCK !!")
+    alert("##STEP-1: Press any button to start the game. ##STEP-2: Click on the flashing box. ##STEP-3: Starting from the first box, keep selecting the flashing boxes in the order, everytime when a new box flashes. ##STEP-4: Remember the sequence and maintain it to keep progressing through levels. ##Eg: ('Red') then ('Red' 'Blue') then ('Red' 'Blue' 'Green') and so on.. GOOD LUCK !!")
 });
