d1c698f2ad610a1ba2db0415257fc25eae76497e
Kshitij Chowdhary <131965273+Kshitij14ry@users.noreply.github.com>
2024-02-19 23:57:12 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,82 @@
+var gamePattern = [];
+var userClickedPattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var started = false;
+var level = 0;
+
+$(document).keypress(function () {
+    if (started == false) {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+
+    var randomNumber = Math.floor((Math.random() * 4));
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor);
+}
+
+$(".btn").click(function () {
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3")
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 50);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] == userClickedPattern[currentLevel]) {
+        console.log("success");
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else {
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startover();
+    }
+}
+
+function startover(){
+    level = 0;
+    started = false;
+    gamePattern = [];
+}
+
+$(".rules").click(function(){
+    alert("##Step-1: Press any button to start the game. ##Step-2: Click on the flashing box. ##Step-3: Start from the first box everytime a new box flashes. ##Step-4: Remember the sequence and maintain it to keep progressing through levels. GOOD LUCK !!")
+});
